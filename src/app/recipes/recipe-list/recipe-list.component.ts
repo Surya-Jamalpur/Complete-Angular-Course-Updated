f71876d4bf5af7492a9165fcae54d926c8db57ac
vishal-gajbeenkar <jsurya352@gmail.com>
2020-06-12 18:25:27 +0530

    onselectrecipe showing the details is completed

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -9,14 +9,17 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 recipes: Recipe[] = [
   new Recipe('the first Recipe', 'the first desc', 'https://health.clevelandclinic.org/wp-content/uploads/sites/3/2019/06/cropped-GettyImages-643764514.jpg'),
-  new Recipe('the first Recipe', 'the first desc', 'https://health.clevelandclinic.org/wp-content/uploads/sites/3/2019/06/cropped-GettyImages-643764514.jpg'),
-  new Recipe('the first Recipe', 'the first desc', 'https://health.clevelandclinic.org/wp-content/uploads/sites/3/2019/06/cropped-GettyImages-643764514.jpg'),
-  new Recipe('the first Recipe', 'the first desc', 'https://health.clevelandclinic.org/wp-content/uploads/sites/3/2019/06/cropped-GettyImages-643764514.jpg'),
-  new Recipe('the first Recipe', 'the first desc', 'https://health.clevelandclinic.org/wp-content/uploads/sites/3/2019/06/cropped-GettyImages-643764514.jpg')
+  new Recipe('the 2nd Recipe', 'the first desc', 'https://health.clevelandclinic.org/wp-content/uploads/sites/3/2019/06/cropped-GettyImages-643764514.jpg'),
+  new Recipe('the 3rd Recipe', 'the first desc', 'https://health.clevelandclinic.org/wp-content/uploads/sites/3/2019/06/cropped-GettyImages-643764514.jpg'),
+  new Recipe('the 4th Recipe', 'the first desc', 'https://health.clevelandclinic.org/wp-content/uploads/sites/3/2019/06/cropped-GettyImages-643764514.jpg'),
+  new Recipe('the 5th Recipe', 'the first desc', 'https://health.clevelandclinic.org/wp-content/uploads/sites/3/2019/06/cropped-GettyImages-643764514.jpg')
 ];
+@Output() recipeWasSelected = new EventEmitter<Recipe>();
   constructor() { }
 
   ngOnInit() {
   }
-
+  onRecipeSelected(currentRecipe:Recipe) {
+    this.recipeWasSelected.emit(currentRecipe);
+  }
 }
