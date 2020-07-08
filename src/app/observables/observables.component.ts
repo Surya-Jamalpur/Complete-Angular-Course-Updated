4650412e87379c813c5681ee59f75f6ccdbbf519
Surya Jamalpuri <SJamalpuri@ad.prolifics.com>
2020-07-08 17:23:14 +0530

    first obs test

--- src/app/observables/observables.component.ts
+++ src/app/observables/observables.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-observables',
   templateUrl: './observables.component.html',
   styleUrls: ['./observables.component.scss']
 })
-export class ObservablesComponent implements OnInit {
+export class ObservablesComponent implements OnInit, OnDestroy {
 
   constructor() { }
-
+private firstObsSubscription:Subscription;
   ngOnInit() {
-  }
-
+    this.firstObsSubscription = interval(500).subscribe(count => {
+      console.log(count);
+    })
+  };
+ngOnDestroy(): void{
+  this.firstObsSubscription.unsubscribe();
+}
 }
