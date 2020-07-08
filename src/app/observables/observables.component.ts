75b34574bb9f0d8734f3f3af47a365ac6391da66
Surya Jamalpuri <SJamalpuri@ad.prolifics.com>
2020-07-08 17:43:26 +0530

    created custom observable and subscribed and unsubscribed manually

--- src/app/observables/observables.component.ts
+++ src/app/observables/observables.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-observables',
@@ -11,11 +11,29 @@ export class ObservablesComponent implements OnInit, OnDestroy {
   constructor() { }
 private firstObsSubscription:Subscription;
   ngOnInit() {
-    this.firstObsSubscription = interval(500).subscribe(count => {
-      console.log(count);
-    })
+    // this is a test driv in observables
+    // this.firstObsSubscription = interval(500).subscribe(count => {
+    //   console.log(count);
+    // });
+
+    // creating custom Observable 
+    const customIntervalObservable = Observable.create(observer => {
+      let x = 1;
+      setInterval(() => {
+        observer.next(x);
+        x++;
+      },1000);
+    });
+    // creating custom Observable ends
+    // Subscribing the observable
+    
+    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+      console.log(data);
+    });
+    // NOTE: subscribe(() => {}) and subscribe(data => {}) both gives the same output
   };
 ngOnDestroy(): void{
+  // we have to unsubscribe custom observable subscription manually like below
   this.firstObsSubscription.unsubscribe();
 }
 }
