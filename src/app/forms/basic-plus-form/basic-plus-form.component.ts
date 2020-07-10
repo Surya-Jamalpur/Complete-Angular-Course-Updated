84c11b9b26002793d941c0533720cc79737c4354
Surya Jamalpuri <SJamalpuri@ad.prolifics.com>
2020-07-10 17:10:04 +0530

    added on two way databinding diffs

--- src/app/forms/basic-plus-form/basic-plus-form.component.ts
+++ src/app/forms/basic-plus-form/basic-plus-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-plus-form',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./basic-plus-form.component.scss']
 })
 export class BasicPlusFormComponent implements OnInit {
-
+  oneway='this is a one way data binding Input..';
+  twoway='this is a Two way data binding Input..';
   constructor() { }
 
   ngOnInit() {
+  }
+  onSubmit(formdata: NgForm){
+    console.log(formdata);
   }
 
 }
